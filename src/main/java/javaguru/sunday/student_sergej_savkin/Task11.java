fee61ddb5bfabd6c3d8a773404bd77a50cf9f64a
Kidrun25 <kidrun25@gmail.com>
2020-09-27 18:23:27 +0300

    Lesson 1; Task_10+11

---
+++ src/main/java/javaguru/sunday/student_sergej_savkin/Task11.java
@@ -0,0 +1,14 @@
+package main.java.javaguru.sunday.student_sergej_savkin;
+
+public class Task11 {
+    public static void main(String[] args) {
+        int x = 74;
+        int y = 36;
+        System.out.println(x + y);
+
+        int a = 50;
+        int b = 3;
+
+        System.out.println(a / b);
+    }
+}
