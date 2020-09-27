4d3e2b326b474fa32759315ec98b33e7c3f69b41
Natalia Kochkina <tisha.silence12@gmail.com>
2020-09-27 14:53:11 +0300

    task_11

--- src/main/java/javaguru/sunday/student_natalia_kochkina/HelloWorld.java
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/HelloWorld.java
@@ -1,8 +1,5 @@
 package main.java.javaguru.sunday.student_natalia_kochkina;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-
-    }
+    public static void main(String[] args) { System.out.println("Hello World!"); }
 }
