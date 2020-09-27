7dcbddd9dbe0981661719b78e424b96552c00a59
pr0h1n <prooohin@gmail.com>
2020-09-27 16:50:43 +0300

    HW10

--- src/student_artjom_proshkin/HelloWorld.java
+++ src/student_artjom_proshkin/HelloWorld.java
@@ -2,6 +2,6 @@ package student_artjom_proshkin;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World !!!");
+        System.out.println("Hello World :) !!!");
     }
 }
