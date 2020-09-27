1e2f85bd75009a2204f0d607b4aad54b448f1f4a
pr0h1n <prooohin@gmail.com>
2020-09-27 17:35:14 +0300

    Task10

---
+++ src/student_artjom_proshkin/L1T11a.java
@@ -0,0 +1,10 @@
+package student_artjom_proshkin;
+
+public class L1T11a {
+    public static void main(String[] args) {
+        int x = 74;
+        int y = 36;
+        int z = x + y;
+        System.out.println(z);
+    }
+}
