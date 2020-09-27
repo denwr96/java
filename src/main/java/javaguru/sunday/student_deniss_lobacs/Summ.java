2be553e883d05108102a61aa19355bea7a85d50e
denwr96 <denwr@inbox.lv>
2020-09-27 12:48:50 +0300

    Task11

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/Summ.java
@@ -0,0 +1,13 @@
+package main.java.javaguru.sunday.student_deniss_lobacs;
+
+public class Summ {
+
+    public static void main(String[] args) {
+
+        System.out.println("Test data:");
+        System.out.println("74+36");
+        System.out.println("Expected Output :");
+        System.out.println("110");
+
+    }
+}
