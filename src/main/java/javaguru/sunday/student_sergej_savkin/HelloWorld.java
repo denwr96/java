d08c4b88d9cf7c276510e751916e46a85950ad96
Kidrun25 <kidrun25@gmail.com>
2020-09-27 12:48:19 +0300

    Lesson 1; Task_10

---
+++ src/main/java/javaguru/sunday/student_sergej_savkin/HelloWorld.java
@@ -0,0 +1,8 @@
+package main.java.javaguru.sunday.student_sergej_savkin;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!!!");
+    }
+}
