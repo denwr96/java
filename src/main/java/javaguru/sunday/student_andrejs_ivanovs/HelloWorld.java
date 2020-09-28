72821385fdc63e898794ef849f350e99fff62844
ivanovsandrej <andrejs.a.ivanovs@accenture.com>
2020-09-28 15:51:00 +0300

    andrejsivanovs_task10

--- src/main/java/javaguru/sunday/student_andrejs_ivanovs/HelloWorld.java
+++ src/main/java/javaguru/sunday/student_andrejs_ivanovs/HelloWorld.java
@@ -2,6 +2,8 @@ package main.java.javaguru.sunday.student_andrejs_ivanovs;
 
 public class HelloWorld {
     public static void main(String[] args) {
+
+        System.out.println("Hello World!!!");
         System.out.println("Hello World!!!");
     }
 }
