f96b26c8b2b66667c5af50b06fa81bf5abbe43a9
KonstantinShestakov <JavaGuru2020>
2020-09-28 13:04:40 +0300

    First commit

--- src/main/java/javaguru/sunday/student_konstantin_shestakov/HelloWorld.java
+++ src/main/java/javaguru/sunday/student_konstantin_shestakov/HelloWorld.java
@@ -3,7 +3,7 @@ package main.java.javaguru.sunday.student_konstantin_shestakov;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!!");
+        System.out.println("Hello World!!!");
     }
 
 }
