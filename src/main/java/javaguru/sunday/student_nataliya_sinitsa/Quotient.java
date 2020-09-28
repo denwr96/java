3acce34c72822f532fc86449afec210f038b1172
sinnaty <nataliasin@gmail.com>
2020-09-28 19:49:25 +0300

    Lesson 1; Task_11

---
+++ src/main/java/javaguru/sunday/student_nataliya_sinitsa/Quotient.java
@@ -0,0 +1,11 @@
+package main.java.javaguru.sunday.student_nataliya_sinitsa;
+
+public class Quotient {
+    public static void main(String[] args) {
+        int divident, division, quotient;
+        divident = 50;
+        division = 3;
+        quotient = divident/division;
+        System.out.println(quotient);
+    }
+}
