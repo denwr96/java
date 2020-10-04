505ac687e7a6ebff094dc61c3cffa9b7b81e13f2
JZUSKO5 <gyera@inbox.lv>
2020-10-04 09:40:15 +0300

    Lesson 1 task 10

---
+++ src/student_jelena_zusko/HelloWorld.java
@@ -0,0 +1,8 @@
+package student_jelena_zusko;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    }
+}
---
+++ src/student_jelena_zusko/HelloWorld2.java
@@ -0,0 +1,5 @@
+package student_jelena_zusko;
+
+public class HelloWorld2 {
+
+}
