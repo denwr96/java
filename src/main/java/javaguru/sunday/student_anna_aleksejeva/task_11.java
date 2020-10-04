884b941b4f122a48793b6007af7452e2546930d2
dzena <dzenaa>
2020-10-04 11:00:36 +0300

    lesson 1; task_10

---
+++ src/main/java/javaguru/sunday/student_anna_aleksejeva/task_11.java
@@ -0,0 +1,8 @@
+package main.java.javaguru.sunday.student_anna_aleksejeva;
+
+public class task_11 {
+    public static void main (String[] args) {
+        System.out.println("74+36=" + (74+36));
+        System.out.println("50/3=" + (50/3));
+    }
+}
