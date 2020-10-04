9a397dd664330db5e92628bf4bc76640a7a31b4a
pr0h1n <prooohin@gmail.com>
2020-10-04 12:12:44 +0300

    Task10and11 + structure change

--- src/main/java/javaguru/sunday/student_artjom_proshkin/lesson_1/HelloWorld.java
+++ src/main/java/javaguru/sunday/student_artjom_proshkin/lesson_1/HelloWorld.java
@@ -1,4 +1,4 @@
-package main.java.javaguru.sunday.student_artjom_proshkin;
+package main.java.javaguru.sunday.student_artjom_proshkin.lesson_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
