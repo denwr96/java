5088a8b8cb723e23b86d82be1505afb8e27c7cfe
Kidrun25 <kidrun25@gmail.com>
2020-10-04 09:45:10 +0300

    Lesson 1; HelloWorld and Task_11

---
+++ src/main/java/javaguru/sunday/student_sergej_savkin/lesson_2/level_5_middle/task_18/SizeMatters.java
@@ -0,0 +1,9 @@
+package main.java.javaguru.sunday.student_sergej_savkin.lesson_2.level_5_middle.task_18;
+
+class SizeMatters {
+    public static void main(String[] args) {
+        System.out.println("Sorry, I have an errors.");
+        System.out.println("Please fix me!");
+    }
+}
+
