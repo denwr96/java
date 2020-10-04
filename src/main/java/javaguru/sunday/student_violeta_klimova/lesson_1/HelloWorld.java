8515694712d5f4a45952a14aa08e2acfe5b7b705
KVioleta <violeta.klimova@gmail.com>
2020-10-04 11:37:58 +0300

    Just rename my own home works and create some folders:)

--- src/main/java/javaguru/sunday/student_violeta_klimova/lesson_1/HelloWorld.java
+++ src/main/java/javaguru/sunday/student_violeta_klimova/lesson_1/HelloWorld.java
@@ -1,4 +1,4 @@
-package main.java.javaguru.sunday.student_violeta_klimova;
+package main.java.javaguru.sunday.student_violeta_klimova.lesson_1;
 
 public class HelloWorld {
     public static void main(String[] args) {
