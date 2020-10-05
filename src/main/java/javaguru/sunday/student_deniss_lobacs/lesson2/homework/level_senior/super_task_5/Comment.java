28d5ff4c19a5a3e69334ae56b106edb0cc2ded2b
denwr96 <denwr@inbox.lv>
2020-10-05 21:34:16 +0300

    SuperTask5

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson2/homework/level_senior/super_task_5/Comment.java
@@ -0,0 +1,14 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson2.homework.level_senior.super_task_5;
+
+public class Comment {
+    public static void main(String[] args) {
+
+        int a=5; // присваиваем переменой a значение 5
+        int b=7; // присваиваем переменой b значение 7
+
+       /*
+        System.out.println("A= " +a);
+        System.out.println("B= " +b);
+          */
+    }
+}
