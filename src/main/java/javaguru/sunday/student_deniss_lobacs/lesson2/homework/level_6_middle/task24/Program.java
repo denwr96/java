d947a52cf11a7ef7036a9a7bb802109aa24d9ca9
denwr96 <denwr@inbox.lv>
2020-10-05 21:20:58 +0300

    Task24

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson2/homework/level_6_middle/task24/Program.java
@@ -0,0 +1,9 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson2.homework.level_6_middle.task24;
+
+public class Program {
+    public static void main(String[] args) {
+        System.out.println("I wanna coffee");
+        System.out.println("So bad");
+    }
+
+}
