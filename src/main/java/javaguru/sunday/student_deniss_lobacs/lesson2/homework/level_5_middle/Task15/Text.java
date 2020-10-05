69343029add5c19f0d0beb9a70ee1ce4ca5e6996
denwr96 <denwr@inbox.lv>
2020-10-05 21:14:11 +0300

    Task15

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson2/homework/level_5_middle/Task15/Text.java
@@ -0,0 +1,9 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson2.homework.level_5_middle.Task15;
+
+public class Text {
+    public static void main(String[] args) {
+        System.out.print("Hello ");
+        System.out.print("world!");
+
+    }
+}
