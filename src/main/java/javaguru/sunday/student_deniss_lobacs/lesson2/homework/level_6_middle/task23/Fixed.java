175e9a9b3119f010e37e27714d7f243ea824f3a8
denwr96 <denwr@inbox.lv>
2020-10-05 21:18:41 +0300

    Task18

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson2/homework/level_6_middle/task23/Fixed.java
@@ -0,0 +1,10 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson2.homework.level_6_middle.task23;
+
+public class Fixed {
+    public static void main(String[] args) {
+
+        System.out.println("Code outside the method will not compile!");
+
+    }
+
+}
