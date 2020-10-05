b6e17687845e1495c957ec2678f464f3c8c46e1c
denwr96 <denwr@inbox.lv>
2020-10-05 20:51:35 +0300

    Task5

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson2/homework/level_2_intern/task5/BankAccount.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson2.homework.level_2_intern.task5;
+
+public class BankAccount {
+    public static void main(String[] args) {
+
+    }
+}
