1b8a99f2c83f5e85ebd66631a58b9b2b76dc9073
denwr96 <denwr@inbox.lv>
2020-10-05 20:56:08 +0300

    Task7

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson2/homework/level_2_intern/task7/LoginService.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson2.homework.level_2_intern.task7;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
