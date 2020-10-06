e0c41faa33afa96c50761cebabe22f9f58193941
Deniss Lenenkov <lenenkov@inbox.lv>
2020-10-06 15:55:30 +0300

    lesson1_task10_task11

---
+++ src/main/java/javaguru/sunday/student_deniss_lenenkov/lesson_1/task_11.java
@@ -0,0 +1,15 @@
+package main.java.javaguru.sunday.student_deniss_lenenkov.lesson_1;
+
+public class task_11 {
+    public static void main(String[] args) {
+        int numberOne = 74;
+        int numberTwo = 36;
+
+        System.out.println(numberOne+numberTwo);
+        //второй пример
+
+        int numberThree = 50;
+        int numberFour = 3;
+        System.out.println(numberThree/numberFour);
+    }
+}
