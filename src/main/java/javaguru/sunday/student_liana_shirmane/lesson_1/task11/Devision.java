2fe81b7753573cb71970838997044a86d78c7263
lianashirmane <liana.sirmane@mintos.com>
2020-10-07 08:54:49 +0300

    Lesson 1 homework

---
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_1/task11/Devision.java
@@ -0,0 +1,12 @@
+package main.java.javaguru.sunday.student_liana_shirmane.lesson_1.task11;
+
+public class Devision {
+    public static void main(String[] args) {
+        int numberOne = 50;
+        int numberTwo = 3;
+
+        int dev = numberOne / numberTwo;
+        System.out.println(dev);
+
+    }
+}
