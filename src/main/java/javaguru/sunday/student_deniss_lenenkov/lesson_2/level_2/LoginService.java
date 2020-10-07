cbbe6785814e89aeee470d3cfd5ba9a458bfa4cb
Deniss Lenenkov <lenenkov@inbox.lv>
2020-10-07 19:20:23 +0300

    Home_work_lesson_2_level_1_2_3

---
+++ src/main/java/javaguru/sunday/student_deniss_lenenkov/lesson_2/level_2/LoginService.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_deniss_lenenkov.lesson_2.level_2;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
