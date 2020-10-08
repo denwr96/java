678f38cf45ee326dda0738641c20cff5ae0ca3a7
denwr96 <denwr@inbox.lv>
2020-10-08 15:43:21 +0300

    Level_3_junior

--- src/main/java/javaguru/sunday/student_deniss_lobacs/lesson2/homework/level_senior/super_task_1/BankAccount.java
+++
@@ -1,7 +0,0 @@
-package main.java.javaguru.sunday.student_deniss_lobacs.lesson2.homework.level_senior.super_task_1;
-
-import main.java.javaguru.sunday.teacher.annotations.CodeReview;
-
-@CodeReview(approved = true)
-public class BankAccount {
-}
---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson_3/homework/level_3_junior/task11/Robot.java
@@ -0,0 +1,20 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson_3.homework.level_3_junior.task11;
+
+public class Robot {
+
+    String name;
+
+    public Robot() {
+
+    }
+
+    public void sayHello() {
+
+        System.out.println("Hello!");
+    }
+
+    public void sayYourName() {
+
+        System.out.println("My name is ROBO");
+    }
+}
