6850b5f4531d3de5ede79f54f0a0e1f7dde6d37c
gsurtajevs <gsurtajevs@gmailcom>
2020-10-09 18:04:00 +0300

    Lesson 2; Task_7

---
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_2/level_2_intern/Task_7/LoginService.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_2.level_2_intern.Task_7;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
