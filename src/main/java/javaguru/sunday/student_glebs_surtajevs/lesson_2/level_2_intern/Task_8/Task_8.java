3f6234c923dd23535b386e13c7c7ec4062462177
gsurtajevs <gsurtajevs@gmailcom>
2020-10-09 18:38:29 +0300

    Lesson 2; Task_8

---
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_2/level_2_intern/Task_8/Task_8.java
@@ -0,0 +1,14 @@
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_2.level_2_intern.Task_8;
+
+import java.util.Scanner;
+
+public class Task_8 {
+    public static void main(String[] args) {
+        Scanner Scanner = new Scanner(System.in);
+        System.out.println("Please enter your name");
+        String name = Scanner.next();
+        System.out.println("Hello " + name);
+
+
+    }
+}
