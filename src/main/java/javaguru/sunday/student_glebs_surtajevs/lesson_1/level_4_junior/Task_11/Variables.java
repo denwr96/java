1b0c029ad164607bb64bd35250673e4e65300b7d
gsurtajevs <gsurtajevs@gmailcom>
2020-10-09 17:31:04 +0300

    Lesson 1; Task_11

--- src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_1/level_4_junior/Task_11/Variables.java
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_1/level_4_junior/Task_11/Variables.java
@@ -1,4 +1,4 @@
-package main.java.javaguru.sunday.student_glebs_surtajevs;
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_1.level_4_junior.Task_11;
 
 import main.java.javaguru.sunday.teacher.annotations.CodeReview;
 
