8c30ef60ed021b344b65de789b649885e5dad781
gsurtajevs <gsurtajevs@gmailcom>
2020-10-09 17:39:01 +0300

    Lesson 1; Task_11

---
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_2/Task_2.java
@@ -0,0 +1,21 @@
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        System.out.println("Please enter first number");
+        Scanner scanner = new Scanner(System.in);
+        double firstNumber = scanner.nextInt();
+        System.out.println("Please enter second number");
+        double secondNumber = scanner.nextInt();
+        double addition = firstNumber + secondNumber;
+        System.out.println("Sum of two numbers =" + addition);
+        double subtraction = firstNumber - secondNumber;
+        System.out.println("Subtraction of two numbers =" + subtraction);
+        double multiplication = firstNumber * secondNumber;
+        System.out.println("Multiplication of two numbers =" + multiplication);
+        double division = firstNumber / secondNumber;
+        System.out.println("Division of two numbers =" + division);
+    }
+}
