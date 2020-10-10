27d2cebe3cbdddbf6be8a4f1efca7132094572d2
ivanovsandrej <andrejs.a.ivanovs@accenture.com>
2020-10-11 02:53:08 +0300

    taks ,9,10, lesson 2

---
+++ src/main/java/javaguru/sunday/student_andrejs_ivanovs/lesson2/Task_9.java
@@ -0,0 +1,25 @@
+package main.java.javaguru.sunday.student_andrejs_ivanovs.lesson2;
+
+import java.util.Scanner;
+
+public class Task_9 {
+    public static void main(String[] args) {
+        Scanner Scan = new Scanner(System.in);
+        System.out.println("Enter your number");
+        int YourNumber = Scan.nextInt();
+
+        System.out.println(YourNumber + " x 1 = " + (YourNumber * 1));
+        System.out.println(YourNumber + " x 2 = " + (YourNumber * 2));
+        System.out.println(YourNumber + " x 3 = " + (YourNumber * 3));
+        System.out.println(YourNumber + " x 4 = " + (YourNumber * 4));
+        System.out.println(YourNumber + " x 5 = " + (YourNumber * 5));
+        System.out.println(YourNumber + " x 6 = " + (YourNumber * 6));
+        System.out.println(YourNumber + " x 7 = " + (YourNumber * 7));
+        System.out.println(YourNumber + " x 8 = " + (YourNumber * 8));
+        System.out.println(YourNumber + " x 9 = " + (YourNumber * 9));
+        System.out.println(YourNumber + " x 10 = " + (YourNumber * 10));
+
+
+
+    }
+}
