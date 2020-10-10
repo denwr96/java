95a6050dc226fb1d158df6a3ec2835c045cad8c0
ivanovsandrej <andrejs.a.ivanovs@accenture.com>
2020-10-11 01:14:43 +0300

    taks 6,7,8, lesson 2

---
+++ src/main/java/javaguru/sunday/student_andrejs_ivanovs/lesson2/LoginService.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_andrejs_ivanovs.lesson2;
+
+public class LoginService {
+    public static void main(String[] args) {
+        System.out.println("You can login!");
+    }
+}
---
+++ src/main/java/javaguru/sunday/student_andrejs_ivanovs/lesson2/Turtle.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_andrejs_ivanovs.lesson2;
+
+public class Turtle {
+    public static void main(String[] args) {
+
+    }
+}
