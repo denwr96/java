7dd277cb013bf40c464ecb78708bd8959ed6e576
Deniss Lenenkov <lenenkov@inbox.lv>
2020-10-10 17:05:28 +0300

    Home_work_level_senior

---
+++ src/main/java/javaguru/sunday/student_deniss_lenenkov/level_senior/LoginService.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_deniss_lenenkov.level_senior;
+
+public class LoginService {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/javaguru/sunday/student_deniss_lenenkov/level_senior/SystemUnit.java
@@ -0,0 +1,7 @@
+package main.java.javaguru.sunday.student_deniss_lenenkov.level_senior;
+
+public class SystemUnit {
+    public static void main(String[] args) {
+
+    }
+}
