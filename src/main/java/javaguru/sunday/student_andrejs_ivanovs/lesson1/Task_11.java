5f48db35d20cf2f00e2ae47de932be30e589d8a9
ivanovsandrej <andrejs.a.ivanovs@accenture.com>
2020-10-10 18:33:23 +0300

    taks 11

---
+++ src/main/java/javaguru/sunday/student_andrejs_ivanovs/lesson1/Task_11.java
@@ -0,0 +1,12 @@
+package main.java.javaguru.sunday.student_andrejs_ivanovs.lesson1;
+
+public class Task_11 {
+    public static void main(String[] args) {
+        int NumberOne = 74;
+        int NumberTwo = 36;
+        int SumOfAllFears = NumberOne + NumberTwo;
+        System.out.println("Sum of 74 and 36 is" + SumOfAllFears);
+
+
+    }
+}
