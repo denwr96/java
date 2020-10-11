296510fc6eb51e5dd61d89e0ab183e67dababcfa
Alex <Wur6un4ik@gmail.com>
2020-10-11 10:49:27 +0300

    Lesson 3: Part 1

---
+++ src/main/java/javaguru/sunday/teacher/lesson_3/lessoncode/Car.java
@@ -0,0 +1,23 @@
+package main.java.javaguru.sunday.teacher.lesson_3.lessoncode;
+
+public class Car {
+
+    public String color;        //color
+    public String manufacture;  //manufacture
+//    public int power;           //power
+//    public double engineVolume; //engineVolume
+//    public String engineType;   //engineType
+
+    public Car() {
+
+    }
+
+    public Car(String color) {
+        this.color = color;
+    }
+
+    public Car(String color, String manufacture) {
+        this.color = color;
+        this.manufacture = manufacture;
+    }
+}
