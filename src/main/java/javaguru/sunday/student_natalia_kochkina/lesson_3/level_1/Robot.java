1e9d2c531fd7560946235576240b5bc559021e39
Natalia Kochkina <tisha.silence12@gmail.com>
2020-10-13 23:32:43 +0300

    homework

---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_3/level_1/Robot.java
@@ -0,0 +1,26 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_3.level_1;
+
+//Task_1
+//Task_2
+//Task_5
+//Task_11
+//Task_12
+//Task_13
+
+public class Robot {
+
+    public String name;
+
+    public Robot(String robotName) {
+        this.name = robotName;
+
+    }
+
+    public void sayHello() {
+        System.out.println("Hello!");
+    }
+    public void sayYourName() {
+        System.out.println("My name is " + this.name);
+    }
+
+}
