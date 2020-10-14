bc43a88aff84e780d472d249e5756e996ab91715
Natalia Kochkina <tisha.silence12@gmail.com>
2020-10-14 22:28:57 +0300

    homework

---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_3/level_4/task_17/BankAccount.java
@@ -0,0 +1,31 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_3.level_4.task_17;
+
+//Task_17
+
+class BankAccount {
+
+    String ownerFirstName;
+    String ownerLastName;
+    int moneyAmount;
+
+    BankAccount(String ownerFirstName,
+                String ownerLastName,
+                int moneyAmount) {
+        this.ownerFirstName = ownerFirstName;
+        this.ownerLastName = ownerLastName;
+        this.moneyAmount = moneyAmount;
+    }
+
+    String getOwnerFirstName() {
+        return this.ownerFirstName;
+    }
+
+    String getOwnerLastName() {
+        return this.ownerLastName;
+    }
+
+    int getMoneyAmount() {
+        return this.moneyAmount;
+    }
+
+}
---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_3/level_5/task_18/DogDemo.java
@@ -0,0 +1,4 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_3.level_5.task_18;
+
+public class DogDemo {
+}
