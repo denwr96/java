9bc8663207b7506d74ca44d2ac2a71a158eafade
pr0h1n <prooohin@gmail.com>
2020-10-14 09:58:10 +0300

    Lesson3 homework level 1 -> level 4

---
+++ src/main/java/javaguru/sunday/student_artjom_proshkin/lesson_3/level_4_junior/task_15/Phone.java
@@ -0,0 +1,15 @@
+package main.java.javaguru.sunday.student_artjom_proshkin.lesson_3.level_4_junior.task_15;
+
+class Phone {
+
+    String model;
+
+        Phone (String newModel) {
+            this.model = newModel;
+        }
+
+        String getModel() {
+            return this.model;
+        }
+
+    }
