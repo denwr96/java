38f861638b4402035af5a44f5f52bda80a5e0fc4
gsurtajevs <gsurtajevs@gmailcom>
2020-10-14 20:16:35 +0300

    Lesson 3; level_1;

--- src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_3/Robot.java
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_3/Robot.java
@@ -1,14 +1,18 @@
 package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_3;
 
-//Task1
-//Task2
-//Task3
-//Task4
-public class Robot {
-    public void sayHello() {
-        System.out.println("Hello!");
-    }
-    public Robot() {
+//Task_1
+//Task_2
+//Task_3
+//Task_4
+//Task_5
+//Task_6
 
+public class Robot {
+
+    public Robot() {
     }
+        public void sayHello() {
+            System.out.println("Hello!");}
+    public void sayYourName() {
+        System.out.println("My name is ROBO");}
 }
--- src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_3/RobotDemo.java
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_3/RobotDemo.java
@@ -1,13 +1,21 @@
 package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_3;
 
-//Task1
-//Task2
-//Task3
-//Task4
+//Task_1
+//Task_2
+//Task_3
+//Task_4
+//Task_5
+//Task_6
+
 public class RobotDemo {
     public static void main(String[] args) {
         Robot myRobot = new Robot();
         myRobot.sayHello();
+        myRobot.sayYourName();
+
+        Robot myRobot2 = new Robot();
+        myRobot2.sayHello();
+        myRobot2.sayYourName();
 
     }
 }
