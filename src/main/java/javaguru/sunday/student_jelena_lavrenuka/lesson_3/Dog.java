2d6f014c916aaa6b09a872d52d5e7099336f5452
jelenalav <funzi@inbox.lv>
2020-10-14 16:23:22 +0300

    lesson_3 - tasks 18-22

---
+++ src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_3/Dog.java
@@ -0,0 +1,32 @@
+package main.java.javaguru.sunday.student_jelena_lavrenuka.lesson_3;
+
+// Task_18
+// Task_19
+// Task_20
+// Task_21
+// Task_22
+
+class Dog {
+    String name;
+    int age;
+    String color;
+
+    Dog(String dogName, int dogAge, String dogColor) {
+        this.name = dogName;
+        this.age = dogAge;
+        this.color = dogColor;
+    }
+
+    void voice() { System.out.println(name + name + name); }
+
+    void voice2() { System.out.println(name + age); }
+
+    void happyBirthday() { System.out.println(age + 1); }
+
+    void voice3() { System.out.println(name + age + color); }
+
+    void voice4() { System.out.println(name + name + name); }
+
+    void changeColor(String newColor) { System.out.println(this.color = newColor); }
+}
+
