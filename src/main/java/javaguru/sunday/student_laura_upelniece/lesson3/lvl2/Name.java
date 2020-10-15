3679e62964c68216744a0e17d73b41cbd2b8c245
LauraProg <laura.upelniece@gamil.com>
2020-10-16 00:23:59 +0300

    Homework

---
+++ src/main/java/javaguru/sunday/student_laura_upelniece/lesson3/lvl2/Name.java
@@ -0,0 +1,8 @@
+package main.java.javaguru.sunday.student_laura_upelniece.lesson3.lvl2;
+
+public class Name {
+    public static void main(String[] args) {
+        String myName = "Laura"; //Creates variable type String
+        System.out.println(myName); //Prints variable myName
+    }
+}
