56e611003064b655afe30f18e0e025095805dfe0
Artjoms Bocarovs <artjoms.bocarovs@neotech.lv>
2020-10-15 15:37:20 +0300

    Added Task 2.

---
+++ src/main/java/javaguru/sunday/student_artjoms_bocarovs/lesson_2/Task_7_LoginService.java
@@ -0,0 +1,11 @@
+package main.java.javaguru.sunday.student_artjoms_bocarovs.lesson_2;
+
+public class Task_7_LoginService {
+
+    public static void main(String[] args) {
+
+        System.out.println("You can login!");
+
+    }
+
+}
---
+++ src/main/java/javaguru/sunday/student_artjoms_bocarovs/lesson_2/Task_8_Hello.java
@@ -0,0 +1,15 @@
+package main.java.javaguru.sunday.student_artjoms_bocarovs.lesson_2;
+
+import java.util.Scanner;
+
+public class Task_8_Hello {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What's your name?");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+
+    }
+}
