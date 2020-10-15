ac118d52a7d356dd94d2463ae06fd709e0c1ed2b
jelenalav <funzi@inbox.lv>
2020-10-15 21:34:43 +0300

    lesson_3 - tasks 25-26

---
+++ src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_3/task_25/Circle.java
@@ -0,0 +1,12 @@
+package main.java.javaguru.sunday.student_jelena_lavrenuka.lesson_3.task_25;
+
+class Circle {
+    double radius;
+
+    Circle(double radius) {
+
+    }
+
+    double calculateArea = Math.PI * Math.pow(2, radius);
+
+}
