ec4f33bdfbef54f5d656a6156759ea5aa94dcf0f
Natalia Kochkina <tisha.silence12@gmail.com>
2020-10-16 23:35:08 +0300

    homework

---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_3/level_6/task_24/WineDemo.java
@@ -0,0 +1,10 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_3.level_6.task_24;
+
+//Task_24
+
+public class WineDemo {
+    public static void main(String[] args) {
+        Wine newWine = new Wine("white", "portveinTriTopora", 13.5);
+        newWine.openTheBottle();
+    }
+}
