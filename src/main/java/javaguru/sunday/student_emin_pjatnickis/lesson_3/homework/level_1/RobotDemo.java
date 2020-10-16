d17ae35344c0e9103c38d82af82d6a6f1c335c14
Emin-Pjatnickis <mrudaf26@gmail.com>
2020-10-16 13:14:37 +0300

    lesson 3   level 1   level 2   level 3   level 4

---
+++ src/main/java/javaguru/sunday/student_emin_pjatnickis/lesson_3/homework/level_1/RobotDemo.java
@@ -0,0 +1,31 @@
+package main.java.javaguru.sunday.student_emin_pjatnickis.lesson_3.homework.level_1;
+
+// level_1
+
+// Task_3
+// Task_4
+// Task_5
+// Task_6
+
+
+// level_3
+
+// Task_12
+// Task_13
+
+public class RobotDemo {
+
+    public static void main(String[] args) {
+
+        Robot robot1 = new Robot("Rider");
+        robot1.sayHello();
+        robot1.sayYourName();
+
+
+        Robot robot2 = new Robot("John");
+        robot2.sayHello();
+        robot2.sayYourName();
+
+    }
+
+}
---
+++ src/main/java/javaguru/sunday/student_emin_pjatnickis/lesson_3/homework/level_4/CarDemo.java
@@ -0,0 +1,18 @@
+package main.java.javaguru.sunday.student_emin_pjatnickis.lesson_3.homework.level_4;
+
+// level_4
+
+// Task_16
+
+public class CarDemo {
+
+    public static void main(String[] args) {
+
+        Car myCar = new Car("Dodge");
+
+        String carModel = myCar.getModel();
+
+        System.out.println("Car model = " + carModel);
+    }
+
+}
