c1583a6e54d4d1bbfec9b2011e54492af8e8d57b
Alex <Wur6un4ik@gmail.com>
2020-10-18 12:30:24 +0300

    Lesson 4: LessonCode

---
+++ src/main/java/javaguru/sunday/teacher/lesson_4/lessoncode/Car.java
@@ -0,0 +1,54 @@
+package main.java.javaguru.sunday.teacher.lesson_4.lessoncode;
+
+public class Car {
+
+    public String man;
+    public int speed;
+    public int openDoors;
+    public int maxDoors;
+    public boolean engine;
+
+    public Car(String man, int maxDoors) {
+        this.man = man;
+        this.speed = 0;
+        this.openDoors = 0;
+        this.maxDoors = maxDoors;
+        this.engine = false;
+    }
+
+    public void setOpenDoors(int numberOfDoorsToOpen) {
+        //Можно ли открыть столько дверей? && Не откроем ли мы больше дверей, чем возможно?
+//        if (maxDoors >= numberOfDoorsToOpen && (openDoors + numberOfDoorsToOpen) <= maxDoors) {
+//            openDoors = numberOfDoorsToOpen;
+//        } else {
+//            System.out.println("There is no available doors");
+//        }
+
+        if (numberOfDoorsToOpen >= maxDoors || (openDoors + numberOfDoorsToOpen) >= maxDoors) {
+            openDoors = maxDoors;
+        } else {
+            openDoors = openDoors + numberOfDoorsToOpen;
+        }
+    }
+
+    public void accelerate() {
+        if (engine && openDoors == 0) {
+            speed = speed + 10;
+            //speed+=10;
+        } else {
+            System.out.println("Engine is OFF OR Doors are open");
+        }
+    }
+
+    public void startStopEngine() {
+//        engine = !engine;
+//        true -> false;
+//        false -> true;
+        if (engine) {
+            engine = false;
+        } else {
+            engine = true;
+        }
+    }
+
+}
