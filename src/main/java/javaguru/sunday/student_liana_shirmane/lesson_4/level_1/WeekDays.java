a6cb6309163da71e9e5f69136f1c4c2f72527cec
lianashirmane <liana.sirmane@mintos.com>
2020-10-21 15:13:09 +0300

    Lesson 4 (level_1 - level_4)

---
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_4/level_1/WeekDays.java
@@ -0,0 +1,27 @@
+package main.java.javaguru.sunday.student_liana_shirmane.lesson_4.level_1;
+//task3
+import java.util.Scanner;
+public class WeekDays {
+    public static void main(String[] args) {
+        System.out.println("Please, enter number from 1 to 7:");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        if (number == 1) {
+            System.out.println("Monday");
+        } else if (number == 2) {
+            System.out.println("Tuesday");
+        } else if (number == 3) {
+            System.out.println("Wednesday");
+        } else if (number == 4) {
+            System.out.println("Thursday");
+        } else if (number == 5) {
+            System.out.println("Friday");
+        } else if (number == 6) {
+            System.out.println("Saturday");
+        } else if (number == 7) {
+            System.out.println("Sunday");
+        } else {
+            System.out.println("Your number doesn't belong to the required interval");
+        }
+    }
+}
