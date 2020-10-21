d11d164234bfb50b05a9b1f67e357c6ee06a7ead
sinnaty <nataliasin@gmail.com>
2020-10-21 20:44:10 +0300

    Lesson 4; Task 17 + Super tasks 1 & 2

---
+++ src/main/java/javaguru/sunday/student_nataliya_sinitsa/lesson4/level6/task17/Stock.java
@@ -0,0 +1,34 @@
+package main.java.javaguru.sunday.student_nataliya_sinitsa.lesson4.level6.task17;
+
+class Stock {
+
+    String name;
+    double currentPrice;
+    double minPrice;
+    double maxPrice;
+
+    public void updatePrice(double updatedPrice) {
+        if (updatedPrice < minPrice) {
+            minPrice = updatedPrice;
+            currentPrice = updatedPrice;
+        } else if (updatedPrice > maxPrice) {
+            maxPrice = updatedPrice;
+            currentPrice = updatedPrice;
+        } else {
+            currentPrice = updatedPrice;
+        }
+    }
+
+    public String getPriceInformation() {
+        return "Company = " + name + ", Current Price = " + currentPrice +
+                ", Min Price = " + minPrice + ", Max Price = " + maxPrice;
+    }
+
+    Stock (String companyName, double productCurrentPrice) {
+        this.name = companyName;
+        this.currentPrice = productCurrentPrice;
+        this.minPrice = productCurrentPrice;
+        this.maxPrice = productCurrentPrice;
+    }
+
+}
