ed0ffb503cbb11005ceafa8e9db7293d3cda75ce
gsurtajevs <gsurtajevs@gmailcom>
2020-10-23 09:57:58 +0300

    Lesson 3; level_6;

---
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_3/level_6/Product.java
@@ -0,0 +1,23 @@
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_3.level_6;
+
+class Product {
+
+    String name;
+    double regularPrice;
+    double discount;
+
+  Product(String name, double regularPrice, double discount) {
+
+      this.name = name;
+      this.regularPrice = regularPrice;
+      this.discount = discount;
+  }
+
+  void printInformation() {
+      double result = regularPrice - (regularPrice * discount);
+      System.out.println("Product: " + name);
+      System.out.println("Regular price: " + regularPrice);
+      System.out.println("Actual price is: " + result);
+  }
+
+}
---
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_3/level_6/ProductDemo.java
@@ -0,0 +1,10 @@
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_3.level_6;
+
+class ProductDemo {
+
+    public static void main(String[] args) {
+
+        Product product = new Product("Steak", 9.00, 0.3);
+        product.printInformation();
+    }
+}
