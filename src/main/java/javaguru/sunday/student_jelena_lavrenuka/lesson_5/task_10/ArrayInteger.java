bde6f70d7b47c0b199d52dcd09b76493d87aca74
jelenalav <funzi@inbox.lv>
2020-10-26 17:43:17 +0200

    lesson_4 - supertask_1 Lesson_5 tasks 10-24

---
+++ src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_5/task_10/ArrayInteger.java
@@ -0,0 +1,13 @@
+package main.java.javaguru.sunday.student_jelena_lavrenuka.lesson_5.task_10;
+
+class ArrayInteger {
+    public static void main(String[] args) {
+        int[] myArrayLenghtThree = new int[3];
+        myArrayLenghtThree[0] = 5;
+        myArrayLenghtThree[1] = 10;
+        myArrayLenghtThree[2] = 15;
+        System.out.println(myArrayLenghtThree[0]);
+        System.out.println(myArrayLenghtThree[1]);
+        System.out.println(myArrayLenghtThree[2]);
+    }
+}
--- src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_5/task_10/ArrayIntiger.java
+++
@@ -1,5 +0,0 @@
-package main.java.javaguru.sunday.student_jelena_lavrenuka.lesson_5.task_10;
-
-class ArrayIntiger {
-
-}
