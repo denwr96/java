6b8db966758b916548a23c91b502308236301806
sinnaty <nataliasin@gmail.com>
2020-11-01 20:04:22 +0200

    Lesson 5; Task 31 - Task 41

---
+++ src/main/java/javaguru/sunday/student_nataliya_sinitsa/lesson5/level5/ArrayUtil.java
@@ -0,0 +1,46 @@
+package main.java.javaguru.sunday.student_nataliya_sinitsa.lesson5.level5;
+
+//Task 31 - Task 41
+
+import java.util.Random;
+
+class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+
+    public void fillArrayWithRandomNumbers(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            Random random = new Random();
+            array[i] = random.nextInt();
+        }
+    }
+
+    public void printArrayToConsole(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Item " + i + " is: " + array[i]);
+        }
+    }
+
+    public int findMaxNumber(int[] array) {
+        int maxNumber = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > maxNumber) {
+                maxNumber = array[i];
+            }
+        }
+        return maxNumber;
+    }
+
+    public int findMinNumber(int[] array) {
+        int minNumber = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < minNumber) {
+                minNumber = array[i];
+            }
+        }
+        return minNumber;
+    }
+
+}
