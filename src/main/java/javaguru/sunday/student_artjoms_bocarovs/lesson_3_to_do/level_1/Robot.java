ecafd6823e09c2ad4c4bf6b1aa4ab05e6096d01b
Artjoms Bocarovs <artjoms.bocarovs@neotech.lv>
2020-11-03 14:35:41 +0200

    Homework

---
+++ src/main/java/javaguru/sunday/student_artjoms_bocarovs/lesson_3_to_do/level_1/Robot.java
@@ -0,0 +1,13 @@
+package main.java.javaguru.sunday.student_artjoms_bocarovs.lesson_3_to_do.level_1;
+
+
+
+public class Robot {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello!");
+
+    }
+
+}
---
+++ src/main/java/javaguru/sunday/student_artjoms_bocarovs/lesson_3_to_do/level_1/RobotDemo.java
@@ -0,0 +1,4 @@
+package main.java.javaguru.sunday.student_artjoms_bocarovs.lesson_3_to_do.level_1;
+
+public class RobotDemo {
+}
