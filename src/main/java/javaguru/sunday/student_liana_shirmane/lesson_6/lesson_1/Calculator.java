0b7a4d9f4147ed26a82952a12fcab116a82acbe6
lianashirmane <liana.sirmane@mintos.com>
2020-11-04 08:25:58 +0200

    Level 1

---
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/lesson_1/Calculator.java
@@ -0,0 +1,9 @@
+package main.java.javaguru.sunday.student_liana_shirmane.lesson_6.lesson_1;
+
+class Calculator {
+
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+}
