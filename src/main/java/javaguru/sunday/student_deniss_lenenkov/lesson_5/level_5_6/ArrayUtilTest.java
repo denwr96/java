825d5132d94d4e8b577bb8b6ab2ac1e756d59938
Deniss Lenenkov <lenenkov@inbox.lv>
2020-11-05 16:12:55 +0200

    home_work_lesson_5_level_5_6

---
+++ src/main/java/javaguru/sunday/student_deniss_lenenkov/lesson_5/level_5_6/ArrayUtilTest.java
@@ -0,0 +1,63 @@
+package main.java.javaguru.sunday.student_deniss_lenenkov.lesson_5.level_5_6;
+
+public class ArrayUtilTest {
+
+    public static void main(String[] args) {
+
+        ArrayUtilTest test = new ArrayUtilTest();
+        test.shouldCreateArray();
+        test.shouldFillArrayWithRandomNumbers();
+        test.shouldFindMaxNumber();
+        test.shouldFindMinNumber();
+
+    }
+    public static void shouldCreateArray(){
+        ArrayUtil myArray = new ArrayUtil();
+        int arrayLength = 3;
+        myArray.createArray(arrayLength);
+        if(arrayLength > 0){
+            System.out.println("shouldCreateArray Test is: " + "OK");
+        }else{
+            System.out.println("shouldCreateArray Test is: " + "FAIL");
+        }
+    }
+    public static void shouldFillArrayWithRandomNumbers(){
+        ArrayUtil victim = new ArrayUtil();
+        int[] myArray = victim.createArray(4);
+        victim.fillArrayWithRandomNumbers(myArray);
+        for(int i = 0; i < myArray.length; i++){
+            check(myArray[i] == myArray[i] , "shouldFillArrayWithRandomNumbers");
+        }
+    }
+
+    public static void shouldFindMaxNumber() {
+        ArrayUtil victim = new ArrayUtil();
+        int[] array = victim.createArray(4);
+        victim.fillArrayWithRandomNumbers(array);
+        int maxNumber = victim.findMaxNumber(array);
+        for(int i = 0; i < array.length;
+        i++){
+            check(maxNumber >= array[i], "shouldFindMaxNUmber");
+        }
+    }
+
+    public static void shouldFindMinNumber() {
+        ArrayUtil victim = new ArrayUtil();
+        int[] array = victim.createArray(4);
+        victim.fillArrayWithRandomNumbers(array);
+        int minNumber = victim.findMinNumber(array);
+        for(int i = 0; i < array.length;
+            i++){
+            check(minNumber <= array[i], "shouldFindMinNUmber");
+        }
+    }
+
+    public static void check(boolean condition, String testName){
+        if(condition){
+            System.out.println(testName + ": OK");
+        }else{
+            System.out.println(testName + ": FAIL");
+        }
+    }
+
+}
