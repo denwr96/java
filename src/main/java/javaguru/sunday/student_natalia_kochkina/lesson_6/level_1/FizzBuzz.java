d16e4b6cc8192bdaae0092bf0752e67b23f54b5c
Natalia Kochkina <tisha.silence12@gmail.com>
2020-11-05 23:51:19 +0200

    homework

---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_6/level_1/FizzBuzz.java
@@ -0,0 +1,14 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_6.level_1;
+
+//Task_3
+
+class FizzBuzz {
+
+    public String detect(int number) {
+        if ((number % 3 == 0) && (number % 5 == 0)) return "FizzBuzz";
+        else if (number % 3 == 0) return "Fizz";
+        else if (number % 5 == 0) return "Buzz";
+        else return "" + number; // Integer.toString(initialNumber);
+    }
+
+}
