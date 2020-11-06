c02690073eca877ffb74161365c8b802b35a6fd4
sinnaty <nataliasin@gmail.com>
2020-11-06 23:50:59 +0200

    Lesson 6; Task 1 - Task 13

---
+++ src/main/java/javaguru/sunday/student_nataliya_sinitsa/lesson6/level1/task1/CalculatorTest.java
@@ -0,0 +1,33 @@
+package main.java.javaguru.sunday.student_nataliya_sinitsa.lesson6.level1.task1;
+
+public class CalculatorTest {
+
+    public static void main(String[] args) {
+        CalculatorTest calculatorTest = new CalculatorTest();
+        calculatorTest.isEvenTest1();
+        calculatorTest.isEvenTest2();
+    }
+
+    public void isEvenTest1() {
+        int number = 8;
+        Calculator calculator = new Calculator();
+        boolean result = calculator.isEven(number);
+        if (result) {
+            System.out.println("1st 'Is even?' test = OK");
+        } else {
+            System.out.println("2nd 'Is even?' test = FAIL");
+        }
+    }
+
+    public void isEvenTest2() {
+        int number = 5;
+        Calculator calculator = new Calculator();
+        boolean result = calculator.isEven(number);
+        if (!result) {
+            System.out.println("2nd 'Is even?' test = OK");
+        } else {
+            System.out.println("2nd 'Is even?' test = FAIL");
+        }
+    }
+
+}
