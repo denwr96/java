3618ce9b3ccb3e059e7397a91f3670189ee9968b
Natalia Kochkina <tisha.silence12@gmail.com>
2020-11-06 14:20:08 +0200

    homework

---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_6/level_4/InfinityWhileExample.java
@@ -0,0 +1,12 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_6.level_4;
+
+//Task_15
+
+public class InfinityWhileExample {
+
+    public static void main(String[] args) {
+        boolean covid19 = true;
+        while (covid19 == true)
+            System.out.println("Не выходить из дома");
+    }
+}
