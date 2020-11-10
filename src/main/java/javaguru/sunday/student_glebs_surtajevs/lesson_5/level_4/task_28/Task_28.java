e0c773f6366bfbf218eb6eecebd9897d127dd4df
gsurtajevs <gsurtajevs@gmailcom>
2020-11-10 22:50:39 +0200

    Lesson 5;

---
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_5/level_4/task_28/Task_28.java
@@ -0,0 +1,25 @@
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_5.level_4.task_28;
+
+import java.util.Random;
+
+public class Task_28 {
+    public static void main(String[] args) {
+            int[] arrayLength = new int[8];
+            Random random = new Random();
+
+            for (int i = 0; i < arrayLength.length; i++) {
+                arrayLength[i] = random.nextInt(100);
+            }
+            for (int k : arrayLength) {
+                System.out.println(k);
+            }
+            int min = arrayLength[0];
+            for (int j : arrayLength) {
+                if (j < min) {
+                    min = j;
+                }
+            }
+            System.out.println("Max number is: " + min);
+
+    }
+}
