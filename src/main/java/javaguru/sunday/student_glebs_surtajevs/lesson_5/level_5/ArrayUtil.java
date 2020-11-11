530b08cb2bd270102317420f1f09644dae306851
gsurtajevs <gsurtajevs@gmailcom>
2020-11-11 22:00:26 +0200

    Lesson 5;

---
+++ src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_5/level_5/ArrayUtil.java
@@ -0,0 +1,24 @@
+package main.java.javaguru.sunday.student_glebs_surtajevs.lesson_5.level_5;
+
+//Task_31
+//Task_32
+//Task_33
+//Task_34
+
+
+import java.util.Random;
+
+class ArrayUtil {
+
+    public int[] createArray(int arrayLength) {
+        return new int[arrayLength];
+    }
+
+    public int[] fillArrayWithRandomNumbers(int[] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        return array;
+    }
+}
