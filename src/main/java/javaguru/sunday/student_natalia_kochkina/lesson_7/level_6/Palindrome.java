cac5541ed66074c8dbd4642cceb66b8b75169473
Natalia Kochkina <tisha.silence12@gmail.com>
2020-11-13 00:50:21 +0200

    homework

---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_7/level_6/Palindrome.java
@@ -0,0 +1,19 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_7.level_6;
+
+//Task_12
+
+public class Palindrome {
+
+    boolean isPalindrome(String text) {
+        text = text.toLowerCase();
+        text = text.replaceAll("[^a-zа-я]", "");
+        char[] textArray = text.toCharArray();
+
+        for (int i = 0; i < textArray.length; i++) {
+            if (textArray[i] != textArray[textArray.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/main/java/javaguru/sunday/student_natalia_kochkina/lesson_7/level_6/PalindromeTest.java
@@ -0,0 +1,32 @@
+package main.java.javaguru.sunday.student_natalia_kochkina.lesson_7.level_6;
+
+//Task_12
+
+public class PalindromeTest {
+
+    public static void main(String[] args) {
+        PalindromeTest palindromeTest = new PalindromeTest();
+        palindromeTest.palindromeTest1();
+        palindromeTest.palindromeTest2();
+    }
+
+    public void palindromeTest1() {
+        Palindrome palindrome = new Palindrome();
+        boolean isPalindrome = palindrome.isPalindrome("what?");
+        if (isPalindrome == false) {
+            System.out.println("Palindrome test1 OK");
+        } else {
+            System.out.println("Palindrome test1 FAILED");
+        }
+    }
+
+    public void palindromeTest2() {
+        Palindrome palindrome = new Palindrome();
+        boolean isPalindrome = palindrome.isPalindrome("А роза упала на лапу Азора");
+        if (isPalindrome == true) {
+            System.out.println("Palindrome test2 OK");
+        } else {
+            System.out.println("Palindrome test2 FAILED");
+        }
+    }
+}
