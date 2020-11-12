dc9db0be70a1615a7dc675a0c44d788419a00ba5
ocean <JavaGuru2020>
2020-11-12 22:31:15 +0200

    12.11.20

--- src/main/java/javaguru/sunday/student_konstantin_shestakov/lesson_6/level_x/super_task_1/lesson_7_lection/House.java
+++
@@ -1,4 +0,0 @@
-package main.java.javaguru.sunday.student_konstantin_shestakov.lesson_6.level_x.super_task_1.lesson_7_lection;
-
-public class House {
-}
--- src/main/java/javaguru/sunday/student_konstantin_shestakov/lesson_6/level_x/super_task_1/lesson_7_lection/InsuredObject.java
+++
@@ -1,8 +0,0 @@
-package main.java.javaguru.sunday.student_konstantin_shestakov.lesson_6.level_x.super_task_1.lesson_7_lection;
-
-import java.math.BigDecimal;
-
-public class InsuredObject {
-
-    private BigDecimal cadastralValue;
-}
