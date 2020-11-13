0987224786505919ae3e35a78a67cb8feaa53a4c
Kristina Sutugina <kri.kor.1992@gmail.com>
2020-11-13 23:25:18 +0200

    Hometask lesson 6

---
+++ src/main/java/javaguru/sunday/student_kristina_sutugina/lesson_6/level_1/Calculator.java
@@ -0,0 +1,11 @@
+package main.java.javaguru.sunday.student_kristina_sutugina.lesson_6.level_1;
+//Task_1
+class Calculator {
+
+    public boolean isEven(int number) {
+            return (number % 2 == 0) ;
+
+
+    }
+
+}
---
+++ src/main/java/javaguru/sunday/student_kristina_sutugina/lesson_6/level_2/WhileDefinition3.java
@@ -0,0 +1,13 @@
+package main.java.javaguru.sunday.student_kristina_sutugina.lesson_6.level_2;
+//Task_6
+class WhileDefinition3 {
+
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 100) {
+            System.out.println(i);
+            i++;
+
+        }
+    }
+}
