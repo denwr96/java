70d5dff9ac61e2e787fb456738ff596a53a56bb3
lianashirmane <liana.sirmane@mintos.com>
2020-11-14 23:27:31 +0200

    Merge remote-tracking branch 'origin/master' into master
    # Conflicts:
    #	src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_4/ForInfLoopForStop.java
    #	src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_4/WhileLoopStop.java
    #	src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_5/TicTacToe.java
    #	src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_5/TicTacToeTest.java

--- src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_5/TicTacToe.java
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_5/TicTacToe.java
@@ -7,20 +7,9 @@ import main.java.javaguru.sunday.teacher.annotations.CodeReview;
 //task21
 //task22
 //task23
+
 @CodeReview(approved = true)
-//task24
-//task25
-//task27
-
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class TicTacToe {
-
-    public static void main(String[] args) {
-        TicTacToe game = new TicTacToe();
-        game.play();
-    }
 
     public boolean isWinPositionForHorizontals(int[][] field, int playerToCheck) {
         for (int i = 0; i < field.length; i++) {
@@ -32,8 +21,10 @@ public class TicTacToe {
     }
 
     public boolean isWinPositionForVerticals(int[][] field, int playerToCheck) {
-        for (int i = 0; i < field.length; i++) {        return true;
-
+        for (int i = 0; i < field.length; i++) {
+            if (field[1][i] == playerToCheck || field[2][i] == playerToCheck || field[3][i] == playerToCheck) {
+                return true;
+            }
         }
         return false;
     }
@@ -50,7 +41,8 @@ public class TicTacToe {
         for (int i = 0; i < field.length; i++) {
             if ((field[i][0] == playerToCheck) && (field[i][1] == playerToCheck) && (field[i][2] == playerToCheck)
                     || (field[0][i] == playerToCheck) && (field[1][i] == playerToCheck) && (field[2][i] == playerToCheck)
-                    || (field[0][2] == playerToCheck) && (field[1][1] == playerToCheck) && (field[2][0] == playerToCheck)) {
+                    || ((field[0][0] == playerToCheck) && (field[1][1] == playerToCheck) && (field[2][2] == playerToCheck)
+                    || (field[0][2] == playerToCheck) && (field[1][1] == playerToCheck) && (field[2][0] == playerToCheck))) {
                 return true;
             }
         }
@@ -60,62 +52,10 @@ public class TicTacToe {
     public boolean isDrawPosition(int[][] field) {
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field.length; j++) {
-                if(field[i][j] == -1){
-                    return false;
-                }
+                if (field[i][j] != -1) ;
             }
+            return true;
         }
-        return true;
-    }
-
-    public int[][] createField() {
-        return new int[][]{{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
-    }
-
-    public Move getNextMove() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter X coordinate");
-        int x = scanner.nextInt();
-        System.out.println("Please enter Y coordinate");
-        int y = scanner.nextInt();
-        Move move = new Move(x, y);
-        return move;
-    }
-
-    public void printFieldToConsole(int[][] field) {
-        for (int[] row : field) {
-            System.out.println(Arrays.toString(row));
-        }
-    }
-
-    public void play() {
-        int[][] field = createField();
-        while (true) {
-            printFieldToConsole(field);
-            Move move0 = getNextMove();
-            field[move0.getX()][move0.getY()] = 0;
-            printFieldToConsole(field);
-            if (isWinPosition(field, 0)) {
-                System.out.println("Player 0 WIN!");
-                break;
-            }
-            if (isDrawPosition(field)) {
-                System.out.println("DRAW!");
-                break;
-            }
-
-            printFieldToConsole(field);
-            Move move1 = getNextMove();
-            field[move1.getX()][move1.getY()] = 1;
-            printFieldToConsole(field);
-            if (isWinPosition(field, 1)) {
-                System.out.println("Player 1 WIN!");
-                break;
-            }
-            if (isDrawPosition(field)) {
-                System.out.println("DRAW!");
-                break;
-            }
-        }
+        return false;
     }
 }
--- src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_5/TicTacToeTest.java
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_6/level_5/TicTacToeTest.java
@@ -6,12 +6,8 @@ import main.java.javaguru.sunday.teacher.annotations.CodeReview;
 //task20
 //task21
 //task22
+
 @CodeReview(approved = true)
-//task23
-//task24
-
-import java.util.Arrays;
-
 public class TicTacToeTest {
     public static void main(String[] args) {
         TicTacToeTest test = new TicTacToeTest();
@@ -20,7 +16,6 @@ public class TicTacToeTest {
         test.winFirstPlayerDiagonals();
         test.winFirstPlayerPosition();
         test.isDraw();
-        test.shouldCreateField();
     }
 
     void winFirstPlayerFirstHorizontalLine() {
@@ -56,14 +51,6 @@ public class TicTacToeTest {
         TicTacToe test = new TicTacToe();
         boolean actualResult = test.isDrawPosition(array);
         check(actualResult, "Nobody win");
-    }
-
-    void shouldCreateField() {
-        TicTacToe test = new TicTacToe();
-        int[][] result = test.createField();
-        int[][] expectedArray = {{-1, -1, -1}, {-1, -1, -1}, {-1, -1, -1}};
-        check(Arrays.deepEquals(expectedArray, result), "create field");
-
     }
 
     public void check(String expectedResult, String actualResult, String testName) {
