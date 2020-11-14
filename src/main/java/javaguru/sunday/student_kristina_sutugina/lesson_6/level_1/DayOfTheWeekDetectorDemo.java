acd37d5f0f9a48f98d6aebb79768b7c267ee868c
Kristina Sutugina <kri.kor.1992@gmail.com>
2020-11-14 19:21:59 +0200

    Hometask lesson 6

---
+++ src/main/java/javaguru/sunday/student_kristina_sutugina/lesson_6/level_1/DayOfTheWeekDetectorDemo.java
@@ -0,0 +1,13 @@
+package main.java.javaguru.sunday.student_kristina_sutugina.lesson_6.level_1;
+
+import java.util.Scanner;
+
+class DayOfTheWeekDetectorDemo {
+    public static void main(String[] args) {
+        DayOfTheWeekDetector dayOfTheWeekDetector = new DayOfTheWeekDetector();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please input a number form 1 till 7: ");
+        int dayNumber = scanner.nextInt();
+        dayOfTheWeekDetector.detectDayName(dayNumber);
+    }
+}
