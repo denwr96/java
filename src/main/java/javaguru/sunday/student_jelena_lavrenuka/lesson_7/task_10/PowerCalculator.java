2327d9ce862294516833d8893a96e7790d46506f
jelenalav <funzi@inbox.lv>
2020-11-18 22:19:44 +0200

    Lesson_7 task 10 update

--- src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_7/task_10/PowerCalculator.java
+++ src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_7/task_10/PowerCalculator.java
@@ -1,10 +1,24 @@
 package main.java.javaguru.sunday.student_jelena_lavrenuka.lesson_7.task_10;
 
 class PowerCalculator {
-    public int PowerCalculator (int number, int degree){
-        int result = 1;
-        for (int i = 0; i < degree; i++) {
-            result = result * number;
-        } return result;
+    public double PowerCalculator(double number, double degree) {
+        if (degree == 0) {
+            return 1;
+        } else if(number == 0){
+            return 0;
+        } else if (degree < 0) {
+            double result = 1;
+            for (double i = 0; i < degree * -1; i++) {
+                result = result * number;
+            }
+            return 1 / result;
+        } else {
+            double result = 1;
+            for (double i = 0; i < degree; i++) {
+                result = result * number;
+            }
+            return result;
+        }
     }
 }
+
--- src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_7/task_10/PowerCalculatorTest.java
+++ src/main/java/javaguru/sunday/student_jelena_lavrenuka/lesson_7/task_10/PowerCalculatorTest.java
@@ -3,17 +3,41 @@ package main.java.javaguru.sunday.student_jelena_lavrenuka.lesson_7.task_10;
 class PowerCalculatorTest {
     public static void main(String[] args) {
         PowerCalculatorTest test = new PowerCalculatorTest();
-        test.PowerCalculatorTesting();
+        test.PowerCalculatorTest1();
+        test.PowerCalculatorTest2();
+        test.PowerCalculatorTest3();
+        test.PowerCalculatorTest4();
 
     }
-    public void PowerCalculatorTesting(){
+    public void PowerCalculatorTest1(){
         PowerCalculator victim = new PowerCalculator();
-        int actualResult = victim.PowerCalculator(2,4);
-        int expectedResult = 16;
-        testResult(actualResult, expectedResult, "PowerCalculatorTest");
+        double actualResult = victim.PowerCalculator(2,4);
+        double expectedResult = 16;
+        testResult(actualResult, expectedResult, "PowerCalculatorTest1");
     }
 
-    public void testResult(int actualResult, int expectedResult, String testName) {
+    public void PowerCalculatorTest2(){
+        PowerCalculator victim = new PowerCalculator();
+        double actualResult = victim.PowerCalculator(2,0);
+        double expectedResult = 1;
+        testResult(actualResult, expectedResult, "PowerCalculatorTest2");
+    }
+
+    public void PowerCalculatorTest3(){
+        PowerCalculator victim = new PowerCalculator();
+        double actualResult = victim.PowerCalculator(2,-3);
+        double expectedResult = 0.125;
+        testResult(actualResult, expectedResult, "PowerCalculatorTest3");
+    }
+
+    public void PowerCalculatorTest4(){
+        PowerCalculator victim = new PowerCalculator();
+        double actualResult = victim.PowerCalculator(0,-3);
+        double expectedResult = 0;
+        testResult(actualResult, expectedResult, "PowerCalculatorTest4");
+    }
+
+    public void testResult(double actualResult, double expectedResult, String testName) {
         if (expectedResult == actualResult) {
             System.out.println(testName + " is OK");
         } else {
