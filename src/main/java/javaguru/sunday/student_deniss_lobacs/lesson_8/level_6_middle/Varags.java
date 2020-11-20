e5ee898cfd0bf45b8fc0ed703e96491b5987325d
denwr96 <denwr@inbox.lv>
2020-11-21 01:00:37 +0200

    Lesson8_level6

---
+++ src/main/java/javaguru/sunday/student_deniss_lobacs/lesson_8/level_6_middle/Varags.java
@@ -0,0 +1,16 @@
+package main.java.javaguru.sunday.student_deniss_lobacs.lesson_8.level_6_middle;
+//task30
+public class Varags {
+    public static void main(String[] args) {
+        Varags varags = new Varags();
+        varags.test(1, 10, 22, -5, 40, 10, 1, 0, -100, 80);
+    }
+
+    public void test(int... args) {
+        int sum = 0;
+        for (int i : args) {
+            sum += 1;
+        }
+        System.out.println("Sum = " + sum);
+    }
+}
