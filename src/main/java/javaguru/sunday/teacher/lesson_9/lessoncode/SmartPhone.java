9d8ab389947d5bafda915d7085e6d24c4a90d3c2
Alex Test <Wur6un4ik@gmail.com>
2020-11-22 13:16:11 +0200

    Lesson 9 Materials

---
+++ src/main/java/javaguru/sunday/teacher/lesson_9/lessoncode/SmartPhone.java
@@ -0,0 +1,53 @@
+package main.java.javaguru.sunday.teacher.lesson_9.lessoncode;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class SmartPhone {
+
+    int pin;
+    String[] smsArray;
+    Photo[] photos;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SmartPhone that = (SmartPhone) o;
+        return pin == that.pin &&
+                Arrays.equals(smsArray, that.smsArray) &&
+                Arrays.equals(photos, that.photos);
+    }
+
+
+    public SmartPhone(int pin) {
+        this.pin = pin;
+    }
+
+    public boolean changePinByUser(int newPin) {
+        if (checkPinCodes()) {
+            changePin(newPin);
+            return true;
+        }
+        return false;
+    }
+
+
+    boolean checkPinCodes() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Type current pint code...");
+        int pinCodeFromUser = scanner.nextInt();
+        return pin == pinCodeFromUser;
+    }
+
+    private void changePin(int newPin) {
+        pin = newPin;
+    }
+
+    //public
+    //protected = default + inheritance
+    //default
+    //private
+
+}
---
+++ src/main/java/javaguru/sunday/teacher/lesson_9/testpackage/HackerSmsService.java
@@ -0,0 +1,15 @@
+package main.java.javaguru.sunday.teacher.lesson_9.testpackage;
+
+import main.java.javaguru.sunday.teacher.lesson_9.lessoncode.SmartPhone;
+
+public class HackerSmsService extends SmartPhone {
+
+    public HackerSmsService(int pin) {
+        super(pin);
+    }
+
+    public void doSomethingWithSMS() {
+//        String[] smsThatSmartPhoneHas = smsArray;
+//        int pinCodeFromParent = pin;
+    }
+}
