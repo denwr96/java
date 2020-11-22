2625ce264a523093a8c29fd2efae72a7f62b63bb
sinnaty <nataliasin@gmail.com>
2020-11-22 12:44:25 +0200

    Lesson 8; Task 15 - 19

---
+++ src/main/java/javaguru/sunday/student_nataliya_sinitsa/lesson8/level34task15/Square.java
@@ -0,0 +1,22 @@
+package main.java.javaguru.sunday.student_nataliya_sinitsa.lesson8.level34task15;
+
+class Square extends Shape {
+
+    private double length;
+
+    Square(String title, double length) {
+        super(title);
+        this.length = length;
+    }
+
+    @Override
+    double calculateArea() {
+        return length * length;
+    }
+
+    @Override
+    double calculatePerimeter() {
+        return 4 * length;
+    }
+
+}
