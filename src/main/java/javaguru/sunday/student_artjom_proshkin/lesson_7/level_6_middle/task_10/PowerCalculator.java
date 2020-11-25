fb6a5114f7b9cfdf9247a8b72c67343fea9c0a2f
pr0h1n <prooohin@gmail.com>
2020-11-25 11:17:32 +0200

    Lesson7 homework

---
+++ src/main/java/javaguru/sunday/student_artjom_proshkin/lesson_7/level_6_middle/task_10/PowerCalculator.java
@@ -0,0 +1,20 @@
+package main.java.javaguru.sunday.student_artjom_proshkin.lesson_7.level_6_middle.task_10;
+
+class PowerCalculator {
+
+    double calculatePowerNumber(double base, double power) {
+        double result = 1;
+        if (power > 0 && base != 0) {
+            for (int i = 0; i < power; i++) {
+                result = result * base;
+            }
+        } else if (power < 0 && base != 0) {
+            for (int i = 0; i > power; i--) {
+                result = result / base;
+            }
+        } else if (power == 0) {
+            return 1;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/javaguru/sunday/student_artjom_proshkin/lesson_7/level_6_middle/task_12/PalindromeTest.java
@@ -0,0 +1,31 @@
+package main.java.javaguru.sunday.student_artjom_proshkin.lesson_7.level_6_middle.task_12;
+
+class PalindromeTest {
+
+    public static void main(String[] args) {
+        PalindromeTest test = new PalindromeTest();
+        test.isPalindromePassTest();
+        test.isPalindromeFailTest();
+
+    }
+
+    void isPalindromePassTest() {
+        Palindrome victim = new Palindrome();
+        boolean actual = victim.isPalindrome("Он дивен палиндром! И ни морд, ни лап не видно.");
+        checkResult(actual, "isPalindromePassTest");
+    }
+    void isPalindromeFailTest() {
+        Palindrome victim = new Palindrome();
+        boolean actual = victim.isPalindrome("Баобаб");
+        checkResult(! actual, "isPalindromeFailTest");
+    }
+
+    void checkResult(boolean condition, String testName) {
+        if (condition) {
+            System.out.println(testName + " PASSED !");
+        } else {
+            System.out.println(testName + " FAILED !");
+        }
+    }
+
+}
