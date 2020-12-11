835f8501f1c9aaedd271c1895ce727a9ce2249e8
lianashirmane <liana.sirmane@mintos.com>
2020-12-11 13:01:28 +0200

    lesson 10 part

---
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_10/level_3/Product2.java
@@ -0,0 +1,16 @@
+package main.java.javaguru.sunday.student_liana_shirmane.lesson_10.level_3;
+//task10
+
+class Product2 {
+
+    private String title;
+
+    public Product2(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+}
---
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_10/level_3/ProductTest2.java
@@ -0,0 +1,40 @@
+package main.java.javaguru.sunday.student_liana_shirmane.lesson_10.level_3;
+//task8
+
+public class ProductTest2 {
+    public static void main(String[] args) {
+        ProductTest2 productTest = new ProductTest2();
+        productTest.shouldFindProduct();
+        productTest.shouldNotFindProduct();
+    }
+    void shouldFindProduct() {
+        Database database = new InMemoryDatabase();
+        Product product1 = new Product("Apple");
+        Product product2 = new Product("Banana");
+        database.save(product1);
+        database.save(product2);
+        Product actualResult = database.findByTitle("Apple");
+        check(product1, actualResult, "Find product in the list");
+    }
+
+    void shouldNotFindProduct() {
+        Database database = new InMemoryDatabase();
+        Product product1 = new Product("Apple");
+        Product product2 = new Product("Banana");
+        database.save(product1);
+        database.save(product2);
+        Product actualResult = database.findByTitle("Carrot");
+        if (actualResult == null) {
+            System.out.println("Do not find product in the list has passed!");
+        } else System.out.println("Do not find product in the list has failed!");
+    }
+    public void check(Product expectedResult, Product actualResult, String testName) {
+        if (expectedResult.equals(actualResult)) {
+            System.out.println(testName + " has passed!");
+        } else {
+            System.out.println(testName + " failed!");
+            System.out.println("Expected: " + expectedResult + " but Actual: " + actualResult);
+        }
+    }
+
+}
