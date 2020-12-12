1724f2dda8dc9a6539563b1678e2dc0de95b8a22
sinnaty <nataliasin@gmail.com>
2020-12-13 00:35:24 +0200

    Lesson 10; Task 14 -> Task 25

---
+++ src/main/java/javaguru/sunday/student_nataliya_sinitsa/lesson10/level6/task14/Book.java
@@ -0,0 +1,57 @@
+package main.java.javaguru.sunday.student_nataliya_sinitsa.lesson10.level6.task14;
+
+//Task 14 -> 25
+
+import java.util.Objects;
+
+public class Book {
+
+    String bookAuthor;
+    String bookTitle;
+    boolean isRead;
+
+    public Book(String bookAuthor, String bookTitle) {
+        this.bookAuthor = bookAuthor;
+        this.bookTitle = bookTitle;
+        this.isRead = false;
+    }
+
+    public Book() {
+
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+        Book book = (Book) o;
+        return bookAuthor.equals(book.bookAuthor) &&
+                bookTitle.equals(book.bookTitle);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookAuthor, bookTitle);
+    }
+
+    @Override
+    public String toString() {
+        return bookTitle + " [" + bookAuthor + "]";
+    }
+}
