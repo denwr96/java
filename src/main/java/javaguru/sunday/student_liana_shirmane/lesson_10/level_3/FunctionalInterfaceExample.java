a4864c7327f1adf38d6099f7402ebc389647efc5
lianashirmane <liana.sirmane@mintos.com>
2020-12-31 21:08:40 +0200

    Corrected home tasks

--- src/main/java/javaguru/sunday/student_liana_shirmane/lesson_10/level_3/FunctionalInterfaceExample.java
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_10/level_3/FunctionalInterfaceExample.java
@@ -2,25 +2,17 @@ package javaguru.sunday.student_liana_shirmane.lesson_10.level_3;
 
 import javaguru.sunday.teacher.annotations.CodeReview;
 import javaguru.sunday.teacher.annotations.CodeReviewComment;
-
+//corrected. Видимо что-то хотела делать и не доделала.
 //task6
 //A functional interface is an interface that contains only one abstract method.
 // They can have only one functionality to exhibit.
 @CodeReview(approved = false)
 @CodeReviewComment(comment = "К чему этот код? =/")
-interface FunctionalInterface {
 
-    public static void main(String args[])
-    {
-        // create anonymous inner class object
-        new Thread(new Runnable()
-        {
-            @Override
-            public void run()
-            {
-                System.out.println("New thread created");
-            }
-        }).start();
-    }
+@FunctionalInterface
+interface FunctionalInterfaceExample {
+
+    String detectDayName(int number);
+
 }
 
