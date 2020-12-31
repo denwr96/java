3e4ce140c62926e583085c0e0a4543bd5edaa5d4
lianashirmane <liana.sirmane@mintos.com>
2020-12-31 23:21:44 +0200

    Level 13 lesson 1

---
+++ src/main/java/javaguru/sunday/student_liana_shirmane/lesson_13/level_1/TaxCalculator.java
@@ -0,0 +1,9 @@
+package javaguru.sunday.student_liana_shirmane.lesson_13.level_1;
+//task1
+
+import java.math.BigDecimal;
+
+interface TaxCalculator {
+
+    BigDecimal calculateTax(BigDecimal income);
+}
