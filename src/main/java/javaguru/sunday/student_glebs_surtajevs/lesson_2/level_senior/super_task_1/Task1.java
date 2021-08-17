0943c0a2193620bebf1fb6d42a2e8e1b0c65eb0a
Denis Lobach <35494145+denwr96@users.noreply.github.com>
2021-08-17 21:29:03 +0300

    Delete src/main/java/javaguru/sunday/student_glebs_surtajevs directory

--- src/main/java/javaguru/sunday/student_glebs_surtajevs/lesson_2/level_senior/super_task_1/Task1.java
+++
@@ -1,7 +0,0 @@
-package javaguru.sunday.student_glebs_surtajevs.lesson_2.level_senior.super_task_1;
-
-import javaguru.sunday.teacher.annotations.CodeReview;
-
-@CodeReview(approved = true)
-public class Task1 {
-}
